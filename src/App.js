3c1afbeee1773f772db4973e03893ddc38e4cab6
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-03-08 00:35:57 +0700

    Setup project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react"
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom"
 import styled, { ThemeProvider, ThemeContext } from "styled-components"
+
+import theme from "./styles/theme"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Switch>
+        <Route path="/"></Route>
+        <Route path="/number1"></Route>
+        <Route path="/number2"></Route>
+      </Switch>
+    </ThemeProvider>
   )
 }
 
---
+++ src/styles/theme.js
@@ -0,0 +1,15 @@
+import breakpoints from "styled-components-breakpoint"
+
+export const theme = {
+  colors: {
+    background_1: `linear-gradient(to top, #e3ab51 , #192c66);`,
+    background_2: `linear-gradient(to top, #c14415 , #2d5251);`
+  },
+  breakpoints: {
+    xs: 0,
+    sm: 576,
+    md: 768,
+    lg: 992,
+    xl: 1200
+  }
+}
