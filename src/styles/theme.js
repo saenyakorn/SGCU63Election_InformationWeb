ca4600a496fa6a6036f9b144356bfd7d6d930b4a
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-03-09 14:11:24 +0700

    Added styles

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,6 +1,6 @@
 import breakpoints from "styled-components-breakpoint"
 
-export const theme = {
+const theme = {
   colors: {
     background_1: `linear-gradient(to top, #e3ab51 , #192c66);`,
     background_2: `linear-gradient(to top, #c14415 , #2d5251);`
@@ -13,3 +13,5 @@ export const theme = {
     xl: 1200
   }
 }
+
+export default theme
